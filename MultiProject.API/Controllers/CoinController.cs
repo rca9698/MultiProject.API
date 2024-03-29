6526dc188e9b408e81e7b7adaa1479fa60f09d90
rca <rca>
2024-03-29 15:15:47 +0530

    code updated

--- MultiProject.API/Controllers/CoinController.cs
+++ MultiProject.API/Controllers/CoinController.cs
@@ -92,29 +92,6 @@ namespace MultiProject.API.Controllers
         }
 
         [HttpPost]
-        [Route("AddCoinsRequest")]
-        public async Task<ReturnType<bool>> AddCoinsRequest(InsertCoinRequestCommand request)
-        {
-            ReturnType<bool> returnType = new ReturnType<bool>();
-
-            if (_userId != request.SessionUser)
-            {
-                returnType.ReturnMessage = "Not a valid session User!!!";
-                return returnType;
-            }
-
-            try
-            {
-                returnType = await _mediator.Send(request);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Exception Occured at CoinController > AddCoinsRequest");
-            }
-            return returnType;
-        }
-
-        [HttpPost]
         [Route("WithDrawCoinsRequest")]
         public async Task<ReturnType<bool>> WithDrawCoinsRequest(DeleteCoinRequestCommand request)
         {
@@ -136,7 +113,6 @@ namespace MultiProject.API.Controllers
             }
             return returnType;
         }
-
 
         [HttpPost]
         [Route("AddCoins")]
